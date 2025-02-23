02bc41f85a750da60dc5550e9a1d0d94e95a7382
Ananth-Pai19 <paiananth0838@gmail.com>
2025-02-23 11:03:45 +0530

    lol
    lol

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
+import express from "express";
+
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  console.log("Hello World");
 });
 
 const PORT = 3000;
